3ad760abe0aeb8857136c4957bddc959009c627a
thilinapoojitha <thilinapoojitha34@gmail.com>
2024-12-10 08:01:33 +0000

    get index file

---
+++ 04-12-2024/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>2020ASP01</h1>
+    <br><br>
+    <a href="gettable.php">Gettable</a>
+    <br>
+</body>
+</html>
