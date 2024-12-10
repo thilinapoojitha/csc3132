b966e121d7f43c62ea66c2f0a242c071bc5b89b3
thilinapoojitha <thilinapoojitha34@gmail.com>
2024-12-10 08:04:40 +0000

    created my function file

---
+++ 04-12-2024/myfunc.php
@@ -0,0 +1,67 @@
+<?php
+require_once 'dbconf.php';
+
+function PrintTable($tableName, $connect) {
+    try {
+        $sql = "SELECT * FROM $tableName";
+        $result = mysqli_query($connect, $sql);
+
+        if (mysqli_num_rows($result) > 0) {
+            echo "<table border=1>";
+
+            $col = mysqli_fetch_fields($result);
+            echo "<tr>";
+            foreach ($col as $value) {
+                echo "<td>" . $value->name . "</td>";
+            }
+            echo "</tr>";
+
+            while ($row = mysqli_fetch_assoc($result)) {
+                echo "<tr>";
+                foreach ($row as $key => $value) {
+                    echo "<td>$value</td>";
+                }
+                echo "</tr>";
+            }
+
+            echo "</table>";
+        } else {
+            echo "No results";
+        }
+    } catch (Exception $e) {
+        die($e->getMessage());
+    }
+}
+
+function SearchName($name, $connect) {
+    try {
+        $sql = "SELECT * FROM student WHERE Name LIKE '%$name%'";
+        $result = mysqli_query($connect, $sql);
+
+        if (mysqli_num_rows($result) > 0) {
+            echo "<table border=1>";
+
+            $col = mysqli_fetch_fields($result);
+            echo "<tr>";
+            foreach ($col as $value) {
+                echo "<td>" . $value->name . "</td>";
+            }
+            echo "</tr>";
+
+            while ($row = mysqli_fetch_assoc($result)) {
+                echo "<tr>";
+                foreach ($row as $key => $value) {
+                    echo "<td>$value</td>";
+                }
+                echo "</tr>";
+            }
+
+            echo "</table>";
+        } else {
+            echo "No results";
+        }
+    } catch (Exception $e) {
+        die($e->getMessage());
+    }
+}
+?>
